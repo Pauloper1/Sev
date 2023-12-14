57be3f5bda98526de5befc0a1dd72b1b63633c98
Pauloper1 <paulcrescent.p@gmail.com>
2023-12-14 13:59:50 +0530

    URL stored in the storage

--- script.js
+++ script.js
@@ -3,8 +3,78 @@ const storageBtn = document.getElementById('storageBtn')
 const tabList = document.getElementById('tabList')
 
 captureBtn.addEventListener('click', function(){
+
+    var currentDate = new Date()
+    currentDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`
+    console.log(`current Date ${currentDate}`)
+    chrome.storage.local.get(currentDate, (result)=>{
+        console.log(`result: `,JSON.stringify(result))
+        var keys = Object.keys(result)
+
+        console.log(`keys: ${keys}`)
+        if(result[0]){
+            //append the new data to current data.
+
+        } else {
+            
+            console.log('else section')
+            getTabs(function (resultData) {
+                const setData = 
+                {
+                    [currentDate]: JSON.stringify(resultData)
+                }
+                console.log('setData')
+                console.log(setData)
+                chrome.storage.local.set(setData, () => {
+                    console.log(`Data saved${JSON.stringify(setData)}`)
+                })
+                chrome.storage.local.get(currentDate,(result_)=>{
+                    console.log('After storing')
+                    console.log(result_)
+                })
+            })
+
+        }
+        console.log('after else section')
+        
+        // var keyExists = keys.some((key)=>{
+        //     return (
+        //         currentDate.getDate() === key.getDate() &&
+        //         currentDate.getMonth() === key.getMonth() &&
+        //         currentDate.getFullYear() === key.getFullYear()
+        //       );
+        // })
+        // if(keyExists){
+            
+        // }
+    })
+
+
+    function getTabs(callback) {
+       
+        chrome.tabs.query({ currentWindow: true }, function (tabs) {
+            var tab = tabs.map(tab => {
+                return ({
+                    title: tab.title,
+                    url: tab.url
+                })
+            })
+            callback(tab)
+        })
+    }
+
+    function getCurrentDate(){
+        
+    }
     chrome.tabs.query({currentWindow: true}, function(tabs){
-        console.log(`tabs: ${JSON.stringify(tabs)}`)
+        var tab = tabs.map(tab => {
+            // console.log({title: tab.title, url: tab.url})
+            return({
+                title: tab.title,
+                url: tab.url
+            })
+        })
+        console.log(`tab: ${JSON.stringify(tab)}`)
         
         tabs.forEach(element => {
             const lielement = document.createElement('li')
@@ -22,3 +92,10 @@ storageBtn.addEventListener('click', function(){
     chrome.tabs.create({url: 'storage.html'})
 })
 
+
+
+//When capture button is pressed
+    // 1. get current date
+    // 2. check whether it's already there or not.
+    // 3. if not, add the date and the associated url.
+    // 4. if already exists, append the urls. 
